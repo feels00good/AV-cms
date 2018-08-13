50ef4188c64e6752e0c39b7b6a20f808383aa730
feels00good <feels00good@ka-as.ru>
2018-08-14 00:26:45 +0300

    Added browserSync

--- Mastering the Modern Workflow/gulpfile.js
+++ Mastering the Modern Workflow/gulpfile.js
@@ -4,26 +4,38 @@ let gulp = require('gulp'),
     autoprefixer = require('autoprefixer'),
     cssvars = require('postcss-simple-vars'),
     nested = require('postcss-nested'),
-    cssImport = require('postcss-import');
+    cssImport = require('postcss-import'),
+    browserSync = require('browser-sync').create();
 
-gulp.task('default', function() {
-  console.log('Hello, friend');
-})
 
 gulp.task('styles', function() {
   return gulp.src('./app/assets/styles/styles.css')
     .pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
     .pipe(gulp.dest('./app/temp/styles'));
 
-})
+});
 
 gulp.task('watch', function() {
+
+  browserSync.init({
+    browser: "google chrome",
+    notify: false,
+    server: {
+      baseDir: "app",
+    }
+  });
+
   watch('./app/index.html', function(){
-    gulp.start('default');
+    browserSync.reload();
   });
 
    watch('./app/assets/styles/**/*.css', function(){
-    gulp.start('styles');
+    gulp.start('cssInject');
   });
 })
+
+gulp.task('cssInject',['styles'], function() {
+  return gulp.src('./app/temp/styles/styles.css')
+    .pipe(browserSync.stream());
+});
 
